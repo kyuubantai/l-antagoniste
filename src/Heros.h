773c069d52316c403d9d28eef8e7564ff382ef83
KOUAME COME p2209838 <come.kouame@etu.univ-lyon1.fr>
2024-03-05 09:07:28 +0000

    Upload New File

---
+++ src/Heros.h
@@ -0,0 +1,13 @@
+#ifndef __IMAGE__H
+#define __IMAGE__H
+
+#include "Personnage.h"
+#include "Objet.h"
+using namespace std;
+
+struct Heros {
+	Personnage heros;
+    Objet invetaire[10];
+};
+
+#endif
