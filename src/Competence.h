3432ffc6cf091d71bede25f92406d2009f2add9b
DUCHAMP LUCAS p2202838 <lucas.duchamp@etu.univ-lyon1.fr>
2024-03-05 07:57:31 +0000

    Upload New File

---
+++ src/Competence.h
@@ -0,0 +1,16 @@
+#ifndef __COMPETENCE__H
+#define __COMPETENCE__H
+
+
+
+class Competence {
+    public:
+        atk(attack,arme,vie_enemie);
+        atk_spe(attack,arme,vie_enemie);
+        soin(vie);
+
+
+};
+
+
+#endif
