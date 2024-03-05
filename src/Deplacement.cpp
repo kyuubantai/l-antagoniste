e51f2deb4cb692665823fa4f650035be876ddb34
DUCHAMP LUCAS p2202838 <lucas.duchamp@etu.univ-lyon1.fr>
2024-03-05 08:50:30 +0000

    Upload New File

---
+++ src/Deplacement.cpp
@@ -0,0 +1,22 @@
+#include "Deplacement.h"
+#include "Monde.h"
+
+Deplacment::deplacement(char event)  {
+    int new_x,new_y;
+    switch (event)
+    {
+        case 'h':
+            new_y=current_y+1;
+        case 'b':
+            new_y=current_y-1;
+        case 'g':
+            new_y=current_x-1;
+        case 'd':
+            new_y=current_x+1;
+        
+    }
+
+    if (tabCase[x][y].libre) {
+        
+    }
+}
