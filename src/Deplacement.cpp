6ec9260d89b84ea2f4b7ecd9bc0be1588ee2c3ba
DUCHAMP LUCAS p2202838 <lucas.duchamp@etu.univ-lyon1.fr>
2024-03-05 09:22:57 +0000

    Replace Deplacement.cpp

--- src/Deplacement.cpp
+++ src/Deplacement.cpp
@@ -2,21 +2,22 @@
 #include "Monde.h"
 
 Deplacment::deplacement(char event)  {
-    int new_x,new_y;
+    int new_x=current_x;
+    int new_y=current_y;
     switch (event)
     {
         case 'h':
-            new_y=current_y+1;
+            new_y+=1;
         case 'b':
-            new_y=current_y-1;
+            new_y-=1;
         case 'g':
-            new_y=current_x-1;
+            new_x-=1;
         case 'd':
-            new_y=current_x+1;
+            new_x+=1;
         
     }
 
-    if (tabCase[x][y].libre) {
-        tabCase[x][y].libre = False;
+    if (tabCase[new_x][new_y].libre) {
+        current_x=new_x;current_y=new_y;
     }
 }
