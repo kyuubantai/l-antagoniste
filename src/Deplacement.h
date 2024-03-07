f579c3749133c0ed21a050a6250f330060384f70
DUCHAMP LUCAS p2202838 <lucas.duchamp@etu.univ-lyon1.fr>
2024-03-07 16:07:04 +0000

    Replace Deplacement.h

--- src/Deplacement.h
+++ src/Deplacement.h
@@ -1,12 +1,11 @@
 #ifndef __DEPLACEMENT__H
 #define __DEPLACEMENT__H
 
-#include "Monde.h"
+#include"Monde.h"
 
 class Deplacement {
     private:
         Monde monde;
-        void interaction;
     public:
         void deplacement(char action,Monde monde);
         void interaction(char action,Monde monde);
