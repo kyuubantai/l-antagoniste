c1f48ef4efde9d961e5a89ea30d306d5b324ad9c
DUCHAMP LUCAS p2202838 <lucas.duchamp@etu.univ-lyon1.fr>
2024-03-07 16:06:42 +0000

    Replace Deplacement.cpp

--- src/Deplacement.cpp
+++ src/Deplacement.cpp
@@ -1,24 +1,30 @@
 #include "Deplacement.h"
 #include "Monde.h"
 
-Deplacement::deplacement(char action,Monde monde)  {
-    int nvX=monde.getX();
-    int nvY=monde.getY();
+
+void Deplacement::deplacement(char action,Monde monde) {
+    int new_x=monde.getX();
+    int new_y=monde.getY();
     switch (action)
     {
         case 'h':
-            nvY+=1;
+            new_y+=1;
         case 'b':
-            nvY-=1;
+            new_y-=1;
         case 'g':
-            nvX-=1;
+            new_x-=1;
         case 'd':
-            nvX+=1;
+            new_x+=1;
         
     }
 
-    if (monde.Carte[0][0].tabCase[nvX][nvY].libre) {
-        monde.setX(nvX);
-        monde.setY(nvY);
+    int carteX=monde.getCarteX();
+    int carteY=monde.getCarteY();
+
+    if (monde.tabCarte[carteX][carteY].tabCase[new_x][new_y].libre) {
+        monde.setX(new_x);
+        monde.setY(new_y);
     }
+}
+void Deplacement::interaction(char action, Monde monde) {
 }
