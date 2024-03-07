4ed79bc3f28b50d533f9c427773e223fa550c47e
KITA DJESSY-ALBERTO p2209697 <djessy-alberto.kita@etu.univ-lyon1.fr>
2024-03-07 14:04:33 +0000

    Upload New File

---
+++ src/Combat.h
@@ -0,0 +1,21 @@
+#ifndef __COMBAT__H
+#define __COMBAT__H
+
+
+#include "Heros.h"
+#include "Ennemi.h"
+
+
+class Combat{
+    private:
+        Heros heros;
+        Ennemi ennemis[3]; // 3 ennemis max dans un combat
+        int nbtour; // le nombre de tour, commencera à 1
+        bool turn; // va déterminer le tour du héros ou des ennemis: true pour les héros, false pour les ennemis
+    public:
+        Combat();
+        ~Combat();
+        int combat();
+};
+
+#endif
