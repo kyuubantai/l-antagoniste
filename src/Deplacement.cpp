146170eb428522049ecabf13980e61811e7193f4
DUCHAMP LUCAS p2202838 <lucas.duchamp@etu.univ-lyon1.fr>
2024-03-12 09:52:32 +0000

    Replace Deplacement.cpp

--- src/Deplacement.cpp
+++ src/Deplacement.cpp
@@ -19,15 +19,40 @@ void Deplacement::deplacement(char action,Monde &monde) {
         case 'd':
             new_y+=1;
             break;
-        
     }
 
     int carteX=monde.getCarte();
 
     if (monde.tabCarte[carteX].tabCase[new_x][new_y].libre) {
-        std::cout<<new_x<<new_y;
+        std::cout<<new_x<<new_y<<std::endl;
         monde.setX(new_x);
         monde.setY(new_y);
+    }
+    if (monde.tabCarte[carteX].tabCase[new_x][new_y].sortie) {
+        std::cout<<"Sortie"<<std::endl;
+        switch (action)
+        {
+            case 'h':
+                new_x=6;
+                monde.setX(new_x);
+                monde.setCarte(carteX-4);
+                break;
+            case 'b':
+                new_x=1;
+                monde.setX(new_x);
+                monde.setCarte(carteX+4);
+                break;
+            case 'g':
+                new_y=10;
+                monde.setY(new_y);
+                monde.setCarte(carteX-1);
+                break;
+            case 'd':
+                new_y=1;
+                monde.setY(new_y);
+                monde.setCarte(carteX+1);
+                break;
+        }
     }
 }
 void Deplacement::interaction(char action, Monde &monde) {
