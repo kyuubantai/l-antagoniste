12011bf5980d83fe8c0e6d247c838afc00b88a21
KITA DJESSY-ALBERTO p2209697 <djessy-alberto.kita@etu.univ-lyon1.fr>
2024-03-19 08:06:11 +0000

    Update file Jeu.cpp

--- A FAIRE/Jeu.cpp
+++ A FAIRE/Jeu.cpp
@@ -1,16 +1,14 @@
 #include "Jeu.h"
 #include <iostream>
-
 using namespace std;
 
-//char image = "data/carte.bmp";
 Jeu::Jeu(): m_window(nullptr),m_surface(nullptr), m_texture(nullptr) {
     
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         cout << "Erreur lors de l'initialisation de la SDL : " << SDL_GetError() << endl;SDL_Quit();exit(1);
     }
 
-    m_window = SDL_CreateWindow("Image", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1200, 1200, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+    m_window = SDL_CreateWindow("L'Antagoniste", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1200, 1200, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
     if (m_window == NULL) { 
         cout << "Erreur lors de la création de la fenetre : " << SDL_GetError() << endl; SDL_Quit(); exit(1);
     }
@@ -27,9 +25,9 @@ Jeu::~Jeu(){
 }
 
 void Jeu::afficher_monde() {
-    //m_surface = SDL_LoadBMP(image);
+    m_surface = SDL_LoadBMP("data/carte.bmp");
     if(m_surface == nullptr) {
-     cout << "Erreur lors du chargement de l'image : " << image <<SDL_GetError();   
+     cout << "Erreur lors du chargement de l'image : " << "data/carte.bmp" <<SDL_GetError();   
     }
     m_texture = SDL_CreateTextureFromSurface(m_renderer,m_surface);
 
@@ -42,16 +40,16 @@ void Jeu::afficher_monde() {
                 case SDL_KEYDOWN:
                     switch(event.key.keysym.sym){
                         case SDLK_LEFT:
-                            d.deplacement("g",m);
+                            d.deplace('g');
                             break;
                         case SDLK_RIGHT:
-                            d.deplacement("d",m);
+                            d.deplace('d');
                             break;
                         case SDLK_UP:
-                            d.deplacement("h",m);
+                            d.deplace('h');
                             break;
                         case SDLK_DOWN:
-                            d.deplacement("b",m);
+                            d.deplace('b');
                             break;
                         default:
                             break;
