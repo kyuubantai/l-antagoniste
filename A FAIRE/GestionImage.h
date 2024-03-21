669ce986ff2dd11df65e5474c54a9b53e0e7369b
KITA DJESSY-ALBERTO p2209697 <djessy-alberto.kita@etu.univ-lyon1.fr>
2024-03-21 14:18:46 +0000

    Update file GestionImage.h

--- A FAIRE/GestionImage.h
+++ A FAIRE/GestionImage.h
@@ -16,14 +16,10 @@ private:
 public:
     GestionImage();
     ~GestionImage();
-    GestionImage& operator=(const GestionImage& gi);
 
-    void loadFromFile (const char* filename, SDL_Renderer * renderer);
-    void loadFromCurrentSurface (SDL_Renderer * renderer);
-    void loadMap(const char* filename, SDL_Renderer * renderer);
+    void loadFile (const char* filename, SDL_Renderer * renderer);
     void draw (SDL_Renderer * renderer, int x, int y, int w=-1, int h=-1);
     SDL_Texture * getTexture() const;
-    void setSurface(SDL_Surface * surf);
 };
-
+inline SDL_Texture * GestionImage::getTexture() const {return m_texture;};
 #endif
