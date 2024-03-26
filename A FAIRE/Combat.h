9e770a64560735fd8731d00d26eba604a6c9fd9c
KOUAME COME p2209838 <come.kouame@etu.univ-lyon1.fr>
2024-03-26 11:35:03 +0000

    Replace Combat.h

--- A FAIRE/Combat.h
+++ A FAIRE/Combat.h
@@ -8,7 +8,8 @@ enum Tour {MOI,ENNEMI};
 class Combat {
     public:
         Tour tour;
-        void combat(Heros heros);
+        void combat(Heros heros,int action);
+        void menuPersoBase(Heros heros);
 
 };
 
