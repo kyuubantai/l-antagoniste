2e51432531124549c6d582f3b52c225137b6ee2a
KITA DJESSY-ALBERTO p2209697 <djessy-alberto.kita@etu.univ-lyon1.fr>
2024-03-26 07:30:35 +0000

    Upload New File

---
+++ src/Jeu.cpp
@@ -0,0 +1,75 @@
+#include "Jeu.h"
+#include <iostream>
+using namespace std;
+
+
+
+
+Jeu::Jeu(): m_window(nullptr),m_surface(nullptr), m_texture(nullptr) {
+    //Création fenêtre
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        cout << "Erreur lors de l'initialisation de la SDL : " << SDL_GetError() << endl;SDL_Quit();exit(1);
+    }
+
+    m_window = SDL_CreateWindow("L'Antagoniste", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 192*5, 112*5, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+    if (m_window == NULL) { 
+        cout << "Erreur lors de la création de la fenetre : " << SDL_GetError() << endl; SDL_Quit(); exit(1);
+    }
+    m_renderer = SDL_CreateRenderer(m_window, -1,SDL_RENDERER_ACCELERATED);
+    //Loads d'image
+    for(int i = 0;i<3;i++){
+    map[i].loadMap(m_renderer,i+1);
+    }
+}
+
+Jeu::~Jeu(){
+    SDL_DestroyWindow(m_window);
+    SDL_DestroyRenderer(m_renderer);
+        if (m_texture != nullptr) {
+            SDL_DestroyTexture(m_texture);
+         }
+         if (m_surface != nullptr) {
+            SDL_FreeSurface(m_surface);
+        }
+    SDL_Quit();
+}
+void Jeu::afficher_monde(){
+    //int x,y;
+    map[0].draw(m_renderer,0,0,192*5,112*5);
+    SDL_RenderPresent(m_renderer);
+}
+void Jeu::boucle_jeu() {
+    bool quit = false;
+    SDL_Event event;
+    while(!quit){
+        while (SDL_PollEvent(&event)) {
+            switch (event.type)
+            {
+                case SDL_KEYDOWN:
+                    switch(event.key.keysym.sym){
+                        case SDLK_LEFT:
+                            d.deplace('g');
+                            break;
+                        case SDLK_RIGHT:
+                            d.deplace('d');
+                            break;
+                        case SDLK_UP:
+                            d.deplace('h');
+                            break;
+                        case SDLK_DOWN:
+                            d.deplace('b');
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                default:
+                    break;
+                }
+                if(event.key.keysym.sym == SDLK_ESCAPE) quit = true;
+        }
+
+        afficher_monde();
+        SDL_RenderPresent(m_renderer);
+    }
+}
