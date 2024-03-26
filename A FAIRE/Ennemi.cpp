5a6d78239ee749f93bb94c23cdc8911ff209b988
KOUAME COME p2209838 <come.kouame@etu.univ-lyon1.fr>
2024-03-26 10:02:11 +0000

    Replace Ennemi.cpp

--- A FAIRE/Ennemi.cpp
+++ A FAIRE/Ennemi.cpp
@@ -7,21 +7,3 @@ Ennemi::Ennemi(){
     stats.setVie(100);
     stats.setAtk(30);
 }
-
-Personnage Ennemi::boss(){
-    stats.setVie(600);
-    stats.setAtk(60);
-}
-
-Personnage Ennemi::tank(){
-    stats.setVie(300);
-    stats.setAtk(40);
-}
-
-Personnage Ennemi::light(){
-    stats.setVie(50);
-    stats.setAtk(15);
-}
-
-
-
