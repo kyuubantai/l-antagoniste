a5e17f55d941908779ad328d4b568a6e0e940663
KITA DJESSY-ALBERTO p2209697 <djessy-alberto.kita@etu.univ-lyon1.fr>
2024-03-26 07:30:48 +0000

    Upload New File

---
+++ src/Jeu.h
@@ -0,0 +1,39 @@
+#ifndef __JEU__H
+#define __JEU__H
+
+#include "Monde.h"
+//#include "Personnage.h"
+//#include "Combat.h"
+#include "Deplacement.h"
+#include "GestionImage.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+class Jeu {
+    private:    
+
+    //lien sur SDL_Window
+        SDL_Window * m_window;
+    //lien sur SDL_Renderer
+        SDL_Renderer * m_renderer;
+    // éventuellement d’autres données (ex. SDL_Surface et SDL_Texture)
+        GestionImage map[9];
+        GestionImage perso;
+        GestionImage ennemi;
+        GestionImage pnj;
+        GestionImage obj;
+        SDL_Surface * m_surface;
+        SDL_Texture * m_texture;
+        bool m_hasChanged;
+        Deplacement d;
+        Monde m;
+    public:
+        Jeu();
+        ~Jeu();
+        void afficher_monde();
+        void boucle_jeu();
+        void boucle_combat();
+
+};
+
+#endif
