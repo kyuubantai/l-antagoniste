80da1ef8eb96896e079fc6735117731bd3ce07fe
KOUAME COME p2209838 <come.kouame@etu.univ-lyon1.fr>
2024-03-26 10:02:51 +0000

    Replace Combat.cpp

--- A FAIRE/Combat.cpp
+++ A FAIRE/Combat.cpp
@@ -1,11 +1,33 @@
+#include "Ennemi.h"
 #include "Combat.h"
 #include <iostream>
 
 using namespace std;
 
-Combat::Combat(){
-    nbtour = 1;
-    turn = true;
-}
+void Combat::combat(Heros heros){
+    bool fin=false;
+    Ennemi ennemi;
+    int n;
+    while(!fin){
+        if(tour == MOI){
+            cout <<"Ton tour"<<endl;
+            cin >> n;
+            ennemi.stats.setVie(ennemi.stats.getVie()-10);
+        }
+        else{
+            cout <<"Ennemi"<<endl;
+            heros.stats.setVie(heros.stats.getVie()-10);
+        }
 
-Combat::~Combat(){};
+        if(heros.stats.getVie()==0 || ennemi.stats.getVie()==0){
+            fin=true;
+        }
+
+        if (tour == MOI)
+		    tour = ENNEMI;
+	    else
+		    tour = MOI;
+    }
+    if(heros.stats.getVie()<=0){cout<<"défaite"<<endl;}
+    if(ennemi.stats.getVie()<=0){cout<<"victoire"<<endl;}
+}
