3b47143bb49857121473884975b5c05cd8d2db24
KITA DJESSY-ALBERTO p2209697 <djessy-alberto.kita@etu.univ-lyon1.fr>
2024-04-02 09:57:57 +0000

    Update file Combat.cpp

--- A FAIRE/Combat.cpp
+++ A FAIRE/Combat.cpp
@@ -1,42 +1,80 @@
-#include "Ennemi.h"
 #include "Combat.h"
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-void Combat::menuPersoBase(Heros heros){
-    int choix
+Combat::Combat(){
+    tour = MOI;
+    ennemis.push_back(en1);
+    ennemis.push_back(en2);
+    ennemis.push_back(en3);
+}
+Ennemi selecteurEnnemi(int pos, vector<Ennemi> ennemis){
+    switch (pos)
+    {
+    case 0:
+        return ennemis[0];
+        break;
+    case 1:
+        return ennemis[1];
+        break;
+    case 2:
+        return ennemis[2];
+        break;
+    default:
+        break;
+    }
+}
+
+//int getAction() return action
+void Combat::menuPersoBase(Heros hero, int action){
+    switch (action)
+    {
+    
+    case 1:  // Attaque basique
+        int action2;
+        break;
+    
+    case 2: // Garde
+
+        break;
+    
+    case 3 : // Compétences
+
+        break;
+
+    case 4 : // Objets
+
+        break;
+    case 0 : // Retraite 
+
+        break;
+    
+    default:
+        break;
+    }
     
 }
 
-void Combat::combat(Heros heros,int action){
+/*void Combat::combat(Heros heros,int action){
     bool fin=false;
     int menu;
     char n;
-    Ennemi ennemi;
     while(!fin){
-        if(tour == MOI){
-            switch (action){
-                case 1:
-                    enn
-                    break;
-                case 2:
-                    break;
-    }
-        }
+        if(tour == MOI) menuPersoBase(heros,action);
         else{
             cout <<"Ennemi"<<endl;
-            heros.stats.setVie(heros.stats.getVie()-10);
+            heros.stats.vie -10;
         }
 
 
         if (tour == MOI) tour = ENNEMI;
 	    else tour = MOI;
-        
-        if(heros.stats.getVie()==0 || ennemi.stats.getVie()==0){
+        if(heros.stats.vie==0 || ennemis[].getVie()==0){
             fin=true;
         }
     }
-    if(heros.stats.getVie()<=0){cout<<"défaite"<<endl;}
-    if(ennemi.stats.getVie()<=0){cout<<"victoire"<<endl;}
-}
+    if(heros.stats.vie<=0){cout<<"défaite"<<endl;}
+    if(en.stats.getVie()<=0){cout<<"victoire"<<endl;}
+}*/
