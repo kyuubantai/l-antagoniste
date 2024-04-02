b0753180c8c6fe328d4d34b45937e726725ff35d
DUCHAMP LUCAS p2202838 <lucas.duchamp@etu.univ-lyon1.fr>
2024-04-02 09:58:56 +0000

    Replace Deplacement.h

--- src/Deplacement.h
+++ src/Deplacement.h
@@ -1,14 +1,19 @@
 #ifndef __DEPLACEMENT__H
 #define __DEPLACEMENT__H
 
-#include"Monde.h"
+#include "Monde.h"
 
 class Deplacement {
     private:
         Monde monde;
+        int num_pnj;
+        int num_dialogue;
     public:
         void deplace(char action);
+        void interaction();
         Monde getMonde() const;
+        int getPnj();
+        int getDialogue();
 };
 
 inline Monde Deplacement::getMonde () const { return monde; }
