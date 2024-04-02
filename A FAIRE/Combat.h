d1aba60a6696b75c566bde561b3b56b527d1bb47
KITA DJESSY-ALBERTO p2209697 <djessy-alberto.kita@etu.univ-lyon1.fr>
2024-04-02 09:58:11 +0000

    Update file Combat.h

--- A FAIRE/Combat.h
+++ A FAIRE/Combat.h
@@ -2,14 +2,22 @@
 #define __COMBAT__H
 
 #include "Heros.h"
+#include "Ennemi.h"
+#include <vector>
+
 
 enum Tour {MOI,ENNEMI};
 
 class Combat {
+    private:
+        Ennemi en1,en2,en3;
+        vector<Ennemi> ennemis;
+        void menuPersoBase(Heros hero,int action);
+        Ennemi selecteurEnnemi(int pos, vector<Ennemi> ennemis);
     public:
+        Combat();
         Tour tour;
         void combat(Heros heros,int action);
-        void menuPersoBase(Heros heros);
 
 };
 
