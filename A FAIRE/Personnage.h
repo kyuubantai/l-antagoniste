4cf2de8012b67f638afd46c4094904ee84f96875
KOUAME COME p2209838 <come.kouame@etu.univ-lyon1.fr>
2024-04-02 07:30:37 +0000

    Replace Personnage.h

--- A FAIRE/Personnage.h
+++ A FAIRE/Personnage.h
@@ -1,15 +1,11 @@
 #ifndef __PERSONNAGE__H
 #define __PERSONNAGE__H
 
-#include "Competence.h"
-using namespace std;
-
 struct Personnage {
 	int vie;
 	int vieMax;
 	int atkBase;
 	int energie;
-	Competence comp[3];
 };
 
 #endif
