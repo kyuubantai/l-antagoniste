1c3947b389235e0d54eaa9833e67b00d272de062
DUCHAMP LUCAS p2202838 <lucas.duchamp@etu.univ-lyon1.fr>
2024-04-08 17:13:01 +0000

    Replace Deplacement.cpp

--- src/Deplacement.cpp
+++ src/Deplacement.cpp
@@ -3,41 +3,62 @@
 #include <iostream>
 
 int Deplacement::getPnj() {
+    int pos_x=monde.getX();
+    int pos_y=monde.getY();
+    int carteX=monde.getCarte();
+    for (int i=-1;i<2;i=i+2) {    
+        if (monde.tabCarte[carteX].tabCase[pos_x+i][pos_y].pnj || monde.tabCarte[carteX].tabCase[pos_x][pos_y+i].pnj) {
+            int pos_s=pos_x+i*100+pos_y;
+            switch(carteX) 
+            {
+                case 0 : if (pos_s==703) {
+                    num_pnj=0;
+                    num_dialogue=0;
+                    break; 
+                    } else if (pos_s==504) {
+                    num_pnj=1;
+                    num_dialogue=0;
+                    break; 
+                }
+                break;
+                case 1 : 
+                    num_pnj=2;
+                    num_dialogue=0;
+                break;
+            }
+        }
+    }
     return num_pnj;
 }
+
+void Deplacement::setPnj(const int& pnj) {
+    num_pnj=pnj;
+}
+
 int Deplacement::getDialogue() {
+    if (num_dialogue != -1) {
+        num_dialogue+=1;
+    }   
     return num_dialogue;
+}
+
+void Deplacement::setDialogue(const int& num_dial) {
+    num_dialogue=num_dial;
 }
 
 void Deplacement::interaction() {
     int pos_x=monde.getX();
     int pos_y=monde.getY();
     int carteX=monde.getCarte();
+    
+    getPnj();
+
     for (int i=-1;i<2;i=i+2) {
         if (monde.tabCarte[carteX].tabCase[pos_x+i][pos_y].ennemi) {
             //Lancé combat avec ennemi (Personnage)
         }
     }
-    for (int i=-1;i<2;i=i+2) {
-        if (monde.tabCarte[carteX].tabCase[pos_x+i][pos_y].pnj) {
-            int pos_s=pos_x+i*100+pos_y;
-            switch(carteX) 
-            {
-                case 0 : if (pos_s==703) {
-                    num_pnj=0;
-                    monde.tabPnj[0].conversation(num_dialogue);
-                    break; 
-                    } else if (pos_s==504) {
-                    num_pnj=1;
-                    monde.tabPnj[1].conversation(num_dialogue);
-                    break; 
-                }
-                break;
-                case 1 : monde.tabPnj[6].conversation(num_dialogue);
-                break;
-            }
-        }
-    }
+    
     for (int i=-1;i<2;i=i+2) {
         if (monde.tabCarte[carteX].tabCase[pos_x+i][pos_y].coffre) {
 
@@ -51,26 +72,6 @@ void Deplacement::interaction() {
         }
     }
 
-    for (int i=-1;i<2;i=i+2) {
-        if (monde.tabCarte[carteX].tabCase[pos_x][pos_y+i].pnj) {
-            int pos_s=pos_x*100+pos_y+i;
-            switch(carteX) 
-            {
-                case 0 : if (pos_s==703) {
-                    num_pnj=0;
-                    monde.tabPnj[0].conversation(num_dialogue);
-                    break; 
-                    } else if (pos_s==504) {
-                    num_pnj=1;
-                    monde.tabPnj[1].conversation(num_dialogue);
-                    break; 
-                }
-                break;
-                case 1 : monde.tabPnj[6].conversation(num_dialogue);
-                break;
-            }
-        }
-    }
 
     for (int i=-1;i<2;i=i+2) {
         if (monde.tabCarte[carteX].tabCase[pos_x][pos_y+i].coffre) {
