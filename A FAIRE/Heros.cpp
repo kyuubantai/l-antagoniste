7ea15509a05fc0e29c874d9ae178411bf1d48636
KOUAME COME p2209838 <come.kouame@etu.univ-lyon1.fr>
2024-04-11 10:24:20 +0000

    Upload New File

---
+++ A FAIRE/Heros.cpp
@@ -0,0 +1,12 @@
+#include "Heros.h"
+#include <iostream>
+
+using namespace std;
+
+Heros::Heros(){
+    stats.vieMax=100;
+    stats.vie=100;
+    stats.atkBase=30;
+    stats.energie=200;
+    stats.def=50;
+}
