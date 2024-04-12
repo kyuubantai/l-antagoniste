5ad0ffd486d832318e4e574596f722970e3b6e64
KOUAME COME p2209838 <come.kouame@etu.univ-lyon1.fr>
2024-04-12 21:52:50 +0000

    Update file Competence.h

--- A FAIRE/Competence.h
+++ A FAIRE/Competence.h
@@ -8,8 +8,29 @@
  **/
 class Competence {
     public:
+
+        /**
+         * @brief Constructeur de la classe : initialise les statistiques des ennemis.
+         * @param vie_ennemi
+         * @param energie
+         * @param atk
+         **/
         int atkMono(int vie_ennemi,int& energie,const int atk);
+
+        /**
+         * @brief Constructeur de la classe : initialise les statistiques des ennemis.
+         * @param vie_ennemi
+         * @param energie
+         * @param atk
+         **/
         int atkMulti(int vie_ennemi,int& energie,const int atk);
+
+        /**
+         * @brief Constructeur de la classe : initialise les statistiques des ennemis.
+         * @param vie_perso
+         * @param energie
+         * @param atk
+         **/
         int soin(int vie_perso,int& energie,const int vieMax_perso);
 
 
