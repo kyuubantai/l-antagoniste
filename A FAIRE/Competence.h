97409529248e0b0581fe26e766901db579bcbb54
KOUAME COME p2209838 <come.kouame@etu.univ-lyon1.fr>
2024-04-12 22:22:29 +0000

    Update file Competence.h

--- A FAIRE/Competence.h
+++ A FAIRE/Competence.h
@@ -11,25 +11,28 @@ class Competence {
 
         /**
          * @brief Constructeur de la classe : initialise les statistiques des ennemis.
-         * @param vie_ennemi
-         * @param energie
-         * @param atk
+         * @param vie_ennemi vie actuelle de l'ennemi
+         * @param energie energie actuelle du heros
+         * @param atk attaque de base du heros
+         * @return la vie de l'ennemi modifiée
          **/
         int atkMono(int vie_ennemi,int& energie,const int atk);
 
         /**
          * @brief Constructeur de la classe : initialise les statistiques des ennemis.
-         * @param vie_ennemi
-         * @param energie
-         * @param atk
+         * @param vie_ennemi vie actuelle de l'ennemi
+         * @param energie energie actuelle du heros
+         * @param atk attaque de base du heros
+         * @return la vie de l'ennemi modifiée
          **/
         int atkMulti(int vie_ennemi,int& energie,const int atk);
 
         /**
          * @brief Constructeur de la classe : initialise les statistiques des ennemis.
-         * @param vie_perso
-         * @param energie
-         * @param atk
+         * @param vie_perso vie actuelle du héros
+         * @param energie energie actuelle du heros
+         * @param vieMax_perso vie maximale du heros
+         * @return la vie du héros modifiée
          **/
         int soin(int vie_perso,int& energie,const int vieMax_perso);
 
