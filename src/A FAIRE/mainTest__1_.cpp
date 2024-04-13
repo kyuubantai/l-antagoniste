8b5bd5a9972faed6aac2eab103f23d479ca37926
KITA DJESSY-ALBERTO p2209697 <djessy-alberto.kita@etu.univ-lyon1.fr>
2024-04-13 17:14:41 +0000

    Upload New File

---
+++ src/A FAIRE/mainTest__1_.cpp
@@ -0,0 +1,25 @@
+#include "Combat.h"
+#include "Heros.h"
+#include <iostream>
+
+using namespace std;
+
+/*enum Etat{MENUBASE,SELECT,COMP,OBJ};
+enum EtatComp{MENUCOMP,MONO,MULTI,HEAL};*/
+
+int main(){
+    Combat c;
+    Heros h;
+    h.stats.atkBase = 20;
+    h.stats.def = 100;
+    h.stats.energie = 250;
+    h.stats.vieMax=999;
+    h.stats.vie = 999;
+    int action;
+    do{
+        c.combat(h,action);
+        cout<<"Choississez votre action : "<<endl;
+        cin>>action;
+    }while(h.stats.vie>0);
+    return 0;
+}
