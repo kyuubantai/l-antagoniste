d071aff6beeeb9af137b6a494ee1f36e88b9e12b
KITA DJESSY-ALBERTO p2209697 <djessy-alberto.kita@etu.univ-lyon1.fr>
2024-04-14 17:37:34 +0000

    Replace Competence.cpp

--- src/Competence.cpp
+++ src/Competence.cpp
@@ -3,19 +3,19 @@
 
 using namespace std;
 
-int Competence::atkMono(int vie_ennemi,int& energie,const int atk){
+int Competence::atkMono(int &vie_ennemi,int& energie,const int atk){
     vie_ennemi -= atk*1.5;
     energie-=20;
     return vie_ennemi;
 }
 
-int Competence::atkMulti(int vie_ennemi,int& energie,const int atk){
+int Competence::atkMulti(int &vie_ennemi,int& energie,const int atk){
     vie_ennemi -= atk*0.7;
     energie-=30;
     return vie_ennemi;
 }
 
-int Competence::soin(int vie_perso, int& energie, const int vieMax_perso){
+int Competence::soin(int &vie_perso, int &energie, const int vieMax_perso){
     vie_perso += 50;
     if (vie_perso>vieMax_perso) vie_perso=vieMax_perso;
     return vie_perso;
