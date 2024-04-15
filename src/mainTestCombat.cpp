7c2df3506c5b8e84eabb79a2f83ea2250193979a
KITA DJESSY-ALBERTO p2209697 <djessy-alberto.kita@etu.univ-lyon1.fr>
2024-04-15 16:51:54 +0000

    Replace mainTestCombat.cpp

--- src/mainTestCombat.cpp
+++ src/mainTestCombat.cpp
@@ -10,11 +10,6 @@ enum EtatComp{MENUCOMP,MONO,MULTI,HEAL};*/
 int main(){
     Combat c;
     Heros h;
-    h.stats.atkBase = 20;
-    h.stats.def = 100;
-    h.stats.energie = 999;
-    h.stats.vieMax=999;
-    h.stats.vie = 999;
     int action;
     do{
         c.combat(action);
